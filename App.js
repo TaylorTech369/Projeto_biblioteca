83d8211d11fdb62eaa92d04808056b0a6eb22466
TaylorTech369 <joaovictormendonca0411@gmail.com>
2026-04-10 13:37:16 -0300

    Recem_Criado

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.caixa_do_meio}>
+
+        <text style={styles.boas_vindas}>Bem vindo ao Projeto Biblioteca</text>
       <StatusBar style="auto" />
+
+      </View>
+      
     </View>
   );
 }
@@ -13,8 +19,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffe8c2',
+    // backgroundColor: '#ffdda8',
+    // backgroundColor: '#D8B589',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    // paddingTop: 50,
   },
+
+  boas_vindas: {
+    // backgroundColor: '#d2a976',
+    fontSize: 32,
+    paddingTop: 10,
+    // color: '#ae8048',
+    color: '#ffc680',
+  },
+
+  caixa_do_meio: {
+    // backgroundColor: '#edc98e',
+    // backgroundColor: '#c19667',
+    backgroundColor: '#a88968',
+    width: '55vw',
+    height: '100vh',
+    alignItems: 'center',
+    borderColor: '#866644',
+    borderWidth: 3,
+  },
+  
 });
